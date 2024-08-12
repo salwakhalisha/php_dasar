11c2bfcd95cbd8ce35b31a89b6d15148be0fe977
unknown <salwakhalisha2023@gmail.com>
2024-08-12 14:10:13 +0700

    belajar hari 1

---
+++ haripertama/coba.php
@@ -0,0 +1,8 @@
+<?php
+
+echo "<h1>Saya nak tanya</h1>";
+print "Saya rase tak da lah lagu macem tu <br>";
+printf("Ish adalah anak saya sering nyanyi <br>");
+
+?>
+<a href="index.php">Halaman index</a>
---
+++ haripertama/index.php
@@ -0,0 +1,11 @@
+<?php
+
+echo "<h1>Saya nak tanya</h1>";
+echo "lagu Da beli da belah <br>";
+print "Saya rase tak da lah lagu macem tu <br>";
+printf("Ish adalah anak saya sering nyanyi <br>");
+echo "___<br>";
+
+?>
+
+<a href="coba.php">Halaman coba.php</a>
