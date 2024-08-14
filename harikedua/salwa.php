adefbc0ce4f6171efdb26792b8b8457aae801a88
unknown <salwakhalisha2023@gmail.com>
2024-08-14 10:34:19 +0700

    coba-coba

---
+++ harikedua/salwa.php
@@ -0,0 +1,19 @@
+<?php
+
+$luas=7; //deklarasi dan inisialisasi variabel
+const phi=3,14; //deklarasi dan inisilisasi konstanta
+
+echo $luas;
+echo "<br>";
+echo phi;
+echo "<br>";
+
+$luasPersegiPanjang=67;
+$kelilingLingkaran=93,5;
+$nama="Salwa";
+$hidup=True;
+
+var_dump($luasPersegiPanjang);
+var_dump($kelilingLingkaran);
+var_dump($nama);
+var_dump($hidup);
